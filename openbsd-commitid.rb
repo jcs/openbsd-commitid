349dbd2c56c22986ad25ea5f9f78489a7bacb0e8
joshua stein <jcs@jcs.org>
2014-08-18 11:06:41 -0500

    add a specific hack for src/gnu/usr.bin/gcc/Attic/INSTALL
    gcc has an old INSTALL file that later became a directory, so
    checking out -r1.1 of the tree will throw an error when it tries to
    mkdir over the existing file.  move it to some other name since it
    was deleted long ago

--- openbsd-commitid.rb
+++ openbsd-commitid.rb
@@ -25,8 +25,15 @@ CVSTREES.each do |tree|
   sc.stray_commitids_to_changesets
   sc.fill_in_changeset_data
 
-  # check out the tree from CVSROOT/#{tree} in a scratch space (CVSTMP), which
-  # is just necessary to be able to issue "cvs admin" commands, which get
-  # stored back in CVSROOT/#{tree}
+  if tree == "src"
+    # gcc has an old INSTALL file that later became a directory, so checking
+    # out -r1.1 of the tree will throw an error when it tries to mkdir over the
+    # existing file.  move it to some other name since it was deleted long ago
+    if File.exists?(CVSROOT + "src/gnu/usr.bin/gcc/Attic/INSTALL,v")
+      system("mv", "-f", CVSROOT + "src/gnu/usr.bin/gcc/Attic/INSTALL,v",
+        CVSROOT + "src/gnu/usr.bin/gcc/Attic/INSTALL.old,v")
+    end
+  end
+
   sc.repo_surgery(CVSTMP, CVSROOT, tree)
 end
